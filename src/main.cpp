c269f265f0ce1f484185b4c56ab9c3f495a2ad4f
Jolaoluwa Amodu <jola47amodu@gmail.com>
2025-07-08 03:04:46 -0500

    Uploaded files for simulation

---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <raylib.h>
+#include <iostream>
+#include "simulation.hpp"
+
+int main()
+{
+    Color GREY = {29, 29, 29, 255};
+    const int WINDOW_WIDTH = 750;
+    const int WINDOW_HEIGHT = 750;
+    const int CELL_SIZE = 25;
+    int FPS = 12;
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Conway's Game of Life");
+    SetTargetFPS(FPS);
+    Simulation simulation{WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE};
+
+    // Simulation Loop
+    while (WindowShouldClose() == false)
+    {
+        // 1. Event Handling
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            Vector2 mouse_position = GetMousePosition();
+            int row = mouse_position.y / CELL_SIZE;
+            int column = mouse_position.x / CELL_SIZE;
+            simulation.ToggleCell(row, column);
+        }
+        if (IsKeyPressed(KEY_ENTER))
+        {
+            simulation.Start();
+            SetWindowTitle("Game of Life is running ...");
+        }
+        else if (IsKeyPressed(KEY_SPACE))
+        {
+            simulation.Stop();
+            SetWindowTitle("Game of Life has stopped. ");
+        }
+        else if (IsKeyPressed(KEY_D))
+        {
+            FPS += 2;
+            SetTargetFPS(FPS);
+        }
+        else if (IsKeyPressed(KEY_A))
+        {
+            if (FPS > 5)
+            {
+                FPS -= 2;
+                SetTargetFPS(FPS);
+            }
+        }
+        else if (IsKeyPressed(KEY_R))
+        {
+            simulation.CreateRandomState();
+        }
+        else if (IsKeyPressed(KEY_C))
+        {
+            simulation.ClearGrid();
+        }
+
+        // 2. Updating State
+        simulation.Update();
+
+        // 3. Drawing
+        BeginDrawing();
+        ClearBackground(GREY);
+        simulation.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
